13baf08f9726d16906f84a9fb4286843b8d6d1ef
CrucifirldOnTheCross <toooriiuum@gmail.com>
2023-09-20 18:05:20 +0400

    second commit

---
+++ MyFirstProgram.java
@@ -0,0 +1,19 @@
+import myfirstpackage.MySecondClass;
+
+class MyFirstClass {
+    public static void main(String[] s) {
+        MySecondClass obj = new MySecondClass();
+        System.out.println(obj.bitOr());
+
+        for (int i = 1; i <= 8; i++) {
+            for (int j = 1; j <= 8; j++) {
+                obj.setNumOne(i);
+                obj.setNumTwo(j);
+                System.out.print(obj.bitOr());
+                System.out.print(" ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
+
